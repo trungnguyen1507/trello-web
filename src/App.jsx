ddba4caccf9cd8f7cf44ad8cfc1c4a08bb71ed98
trungndse150553 <trungndse150553@fpt.edu.vn>
2023-12-18 09:10:31 +0700

    Dark Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  // function useColorScheme đã hỗ trợ lưu giá trị theme mode vào localStorage
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+
       <div>trungnguyen</div>
 
       <Typography variant='body2' color='text.secondary'>
