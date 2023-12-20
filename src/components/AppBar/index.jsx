9d120201b504232f503cc7d728bbb48c59c65db2
trungndse150553 <trungndse150553@fpt.edu.vn>
2023-12-20 15:53:01 +0700

    Code App Bar P2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -30,29 +30,36 @@ function AppBar() {
     >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
+
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <SvgIcon component={TrelloIcon} fontSize='small' inheritViewBox sx={{ color: 'primary.main' }} />
           <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>
             Trello
           </Typography>
         </Box>
+
         <Workspaces />
         <Recent />
         <Starred />
         <Templates />
+
         <Button variant='outlined'>Create</Button>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <TextField id='outlined-search' label='Search ...' type='search' size='small' />
+
         <ModeSelect />
+
         <Tooltip title='Notifications'>
           <Badge color='secondary' variant='dot' sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }} />
           </Badge>
         </Tooltip>
+
         <Tooltip title='Help'>
-          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
         </Tooltip>
+
         <Profiles />
       </Box>
     </Box>
