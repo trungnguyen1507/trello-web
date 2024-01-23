637dd8c74b100d2eb399cfe50d934b655752a8d7
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-01-23 11:25:33 +0700

    Query data

--- src/pages/Boards/BoardDetail.jsx
+++ src/pages/Boards/BoardDetail.jsx
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
 
@@ -10,7 +10,7 @@ function Board() {
   const [board, setBoard] = useState([])
 
   useEffect(() => {
-    const boardId = '65ac8f7c118af0523f1c473e'
+    const boardId = '65af25940fb19081d2cd07b5'
     fetchBoardDetailsAPI(boardId).then((board) => {
       setBoard(board)
     })
